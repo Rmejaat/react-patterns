88cb323079065e39e561c904404f1e39710f5de7
Rayan Mejaat <142797698+HandiReaper@users.noreply.github.com>
2025-05-31 14:12:58 +0200

    Exercice 4

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -1,7 +1,6 @@
 // Props Drilling
 // http://localhost:3000/alone/exercise/04.js
 /* eslint-disable no-unused-vars */
-import * as React from 'react'
 
 // 🐶 Fais évoluer le Header en ajoutant en deux composants enfants <Login /> <MenuBarInfo />
 // Login retourne <div>Connexion au site : {siteName} </div>
@@ -9,44 +8,58 @@ import * as React from 'react'
 
 // 🐶 Tu vas devoir faire transiter les props depuis App -> Header -> Login/MenuBarInfo
 
-function Header({siteName}) {
-  return <h1>Bienvenue sur {siteName} </h1>
+const Login = ({ siteName }) => {
+  return <div>Connexion au site : {siteName} </div>
 }
-function Content({siteName}) {
-  return <div>Article du site {siteName} </div>
+
+const MenuBarInfo = ({ nbMessages }) => {
+  return <div>Vous avez {nbMessages} messages</div>
 }
-function Footer({siteName}) {
+
+function Header({ siteName, nbMessages }) {
   return (
     <>
-      <CGV siteName={siteName} />
-      <About siteName={siteName} />
+      <h1>Bienvenue sur {siteName} </h1>
+      <Login siteName={siteName} />
+      <MenuBarInfo nbMessages={nbMessages} />
     </>
   )
 }
-function CGV({siteName}) {
+function Content({ siteName }) {
+  return <div>Article du site {siteName} </div>
+}
+function Footer({ siteName, email }) {
+  return (
+    <>
+      <CGV siteName={siteName} />
+      <About siteName={siteName} email={email} />
+    </>
+  )
+}
+function CGV({ siteName }) {
   return <div>CGV du site : {siteName} </div>
 }
 
-function About({siteName}) {
+function About({ siteName, email }) {
   // 🐶 Tu vas devoir faire évoluer <About> pour qu'il utilise l'email de App
   // 🤖 <div>Contactez nous : {email}</div>
   return (
     <>
       <div>A propos du site : {siteName} </div>
-      <div>Contactez nous : contact@mikecodeur.com</div>
+      <div>Contactez nous : {email}</div>
     </>
   )
 }
 
 function App() {
-  const siteName = 'mikecodeur.com'
-  const email = 'contact@mikecodeur.com'
+  const siteName = "mikecodeur.com"
+  const email = "contact@mikecodeur.com"
   const nbMessages = 18
   return (
     <>
-      <Header siteName={siteName} />
+      <Header siteName={siteName} nbMessages={nbMessages} />
       <Content siteName={siteName} />
-      <Footer siteName={siteName} />
+      <Footer siteName={siteName} email={email} />
     </>
   )
 }
